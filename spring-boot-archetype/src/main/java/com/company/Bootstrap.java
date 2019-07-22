dd758adf080124553d863bb1079c7922e0fd368f
chendehe <chendehe22@163.com>
2019-07-22 15:29:40 +0800

    druid 配置

--- spring-boot-archetype/src/main/java/com/company/Bootstrap.java
+++ spring-boot-archetype/src/main/java/com/company/Bootstrap.java
@@ -2,11 +2,13 @@ package com.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * Springboot启动类
  */
 @SpringBootApplication
+@ServletComponentScan
 public class Bootstrap {
 
   public static void main(String[] args) {
