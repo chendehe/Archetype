b22d8275ad538776a556ab28273d629e0a1c78ee
chendehe <chendehe22@163.com>
2019-07-23 01:10:27 +0800

    更新类名

--- spring-boot-archetype/src/main/java/com/company/config/UserProperties.java
+++ spring-boot-archetype/src/main/java/com/company/config/UserProperties.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 /**
  * 自定义配置样例.
@@ -13,10 +13,10 @@ import org.springframework.context.annotation.PropertySource;
 @Getter
 @Setter
 @ToString
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "user")
 @PropertySource("classpath:user.properties")
-public class UserConfig {
+public class UserProperties {
   private String username;
   private Integer age;
 }
