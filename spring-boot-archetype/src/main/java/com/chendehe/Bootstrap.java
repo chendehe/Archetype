a47d4c258ba6d7f1869e6744f1c4af076527bd4e
chendehe <chendehe22@163.com>
2019-07-23 17:59:54 +0800

    CORS

--- spring-boot-archetype/src/main/java/com/chendehe/Bootstrap.java
+++ spring-boot-archetype/src/main/java/com/chendehe/Bootstrap.java
@@ -3,6 +3,9 @@ package com.chendehe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Springboot启动类
@@ -15,4 +18,16 @@ public class Bootstrap {
     SpringApplication.run(Bootstrap.class, args);
   }
 
+  /**
+   * 定义全局的 CORS，也可以在 controller 注解 @CrossOrigin.
+   */
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**");
+      }
+    };
+  }
 }
