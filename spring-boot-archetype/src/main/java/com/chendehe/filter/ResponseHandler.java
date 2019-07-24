fa8465133cd932ecc51aa8acb69853819af8ff35
chendehe <chendehe22@163.com>
2019-07-24 15:07:30 +0800

    使用 slf4j+Logback 日志

--- spring-boot-archetype/src/main/java/com/chendehe/filter/ResponseHandler.java
+++ spring-boot-archetype/src/main/java/com/chendehe/filter/ResponseHandler.java
@@ -5,6 +5,8 @@ import com.chendehe.exception.BaseException;
 import com.chendehe.exception.ErrorMessage;
 import com.chendehe.exception.ErrorResult;
 import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,9 +20,12 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice("com.chendehe.controller")
 public class ResponseHandler extends ResponseEntityExceptionHandler {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(ResponseHandler.class);
+
   @ResponseBody
   @ExceptionHandler(Exception.class)
   public ResponseEntity handleControllerException(HttpServletRequest request, Throwable ex) {
+    LOGGER.error("occur error", ex);
     if (ex instanceof BaseException) {
       return new ResponseEntity<>(new ErrorResult(ex.getMessage(), ex.getLocalizedMessage()), HttpStatus.BAD_REQUEST);
     } else {
@@ -30,6 +35,7 @@ public class ResponseHandler extends ResponseEntityExceptionHandler {
 
   private HttpStatus getStatus(HttpServletRequest request) {
     Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+    LOGGER.debug("http status is:{}", statusCode);
     if (statusCode == null) {
       return HttpStatus.INTERNAL_SERVER_ERROR;
     }
