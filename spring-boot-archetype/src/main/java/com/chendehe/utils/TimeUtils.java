b3b3bb205e57040dace832de6972177b2852b46c
chendehe <chendehe22@163.com>
2019-07-26 15:41:13 +0800

    时间工具类

--- spring-boot-archetype/src/main/java/com/chendehe/utils/TimeUtils.java
+++ spring-boot-archetype/src/main/java/com/chendehe/utils/TimeUtils.java
@@ -1,11 +1,15 @@
 package com.chendehe.utils;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjusters;
 
 /**
  * 时间工具类.
@@ -21,12 +25,12 @@ public final class TimeUtils {
   /**
    * Temporal 转为字符串.
    */
-  public static <T extends Temporal> String parseString(T time, String... format) {
+  public static String parseString(Temporal time, String... format) {
     if (time instanceof LocalDate) {
-      String dateFormat = 0 == format.length ? DATE : format[0];
+      String dateFormat = null == format || 0 == format.length ? DATE : format[0];
       return ((LocalDate) time).format(DateTimeFormatter.ofPattern(dateFormat));
     } else if (time instanceof LocalDateTime) {
-      String dateFormat = 0 == format.length ? DATE_TIME : format[0];
+      String dateFormat = null == format || 0 == format.length ? DATE_TIME : format[0];
       return ((LocalDateTime) time).format(DateTimeFormatter.ofPattern(dateFormat));
     }
 
@@ -37,7 +41,7 @@ public final class TimeUtils {
    * 字符串转为 LocalDate.
    */
   public static LocalDate parseLocalDate(String time, String... format) {
-    String dateFormat = 0 == format.length ? DATE : format[0];
+    String dateFormat = null == format || 0 == format.length ? DATE : format[0];
     return LocalDate.parse(time, DateTimeFormatter.ofPattern(dateFormat));
   }
 
@@ -45,8 +49,15 @@ public final class TimeUtils {
    * 字符串转为 LocalDateTime.
    */
   public static LocalDateTime parseLocalDateTime(String time, String... format) {
-    String dateFormat = 0 == format.length ? DATE_TIME : format[0];
+    String dateFormat = null == format || 0 == format.length ? DATE_TIME : format[0];
     return LocalDateTime.parse(time, DateTimeFormatter.ofPattern(dateFormat));
+  }
+
+  /**
+   * Long 转为 LocalDateTime.
+   */
+  public static LocalDateTime parseLocalDateTime(long timestamp) {
+    return LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
   }
 
   /**
@@ -57,10 +68,77 @@ public final class TimeUtils {
   }
 
   /**
-   * Long 转为 LocalDateTime.
+   * 当前月第一天.
    */
-  public static LocalDateTime parseLocalDateTime(long timestamp) {
-    return LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
+  public static Temporal startDayOfMonth(Temporal time) {
+    if (time instanceof LocalDate) {
+      return ((LocalDate) time).with(TemporalAdjusters.firstDayOfMonth());
+    } else if (time instanceof LocalDateTime) {
+      return ((LocalDateTime) time).with(TemporalAdjusters.firstDayOfMonth());
+    }
+    return null;
+  }
+
+  /**
+   * 当前月最后一天.
+   */
+  public static Temporal endDayOfMonth(Temporal time) {
+    if (time instanceof LocalDate) {
+      return ((LocalDate) time).with(TemporalAdjusters.lastDayOfMonth());
+    } else if (time instanceof LocalDateTime) {
+      return ((LocalDateTime) time).with(TemporalAdjusters.lastDayOfMonth());
+    }
+    return null;
+  }
+
+  /**
+   * 当前月第 N 天.
+   */
+  public static Temporal dayOfMonth(Temporal time, int n) {
+    if (time instanceof LocalDate) {
+      return ((LocalDate) time).withDayOfMonth(n);
+    } else if (time instanceof LocalDateTime) {
+      return ((LocalDateTime) time).withDayOfMonth(n);
+    }
+    return null;
+  }
+
+  /**
+   * 当前时间 +/- n 天.
+   */
+  public static Temporal plusDay(Temporal time, int n) {
+    if (time instanceof LocalDate) {
+      return ((LocalDate) time).plusDays(n);
+    } else if (time instanceof LocalDateTime) {
+      return ((LocalDateTime) time).plusDays(n);
+    }
+    return null;
+  }
+
+  /**
+   * 当前时间 +/- n 小时.
+   */
+  public static Temporal plusHours(Temporal time, int n) {
+    if (time instanceof LocalTime) {
+      return ((LocalTime) time).plusHours(n);
+    } else if (time instanceof LocalDateTime) {
+      return ((LocalDateTime) time).plusHours(n);
+    }
+    return null;
+  }
+
+  /**
+   * LocalTime/LocalDateTime的时间差：(time2-time1)秒.
+   */
+  public static long getSeconds(Temporal time1, Temporal time2) {
+    return Duration.between(time1, time2).getSeconds();
+  }
+
+  /**
+   * 时间差：(date2-date1)天.
+   */
+  public static long getDays(LocalDate date1, LocalDate date2) {
+    return Period.between(date1, date2).getDays();
   }
 
 }
