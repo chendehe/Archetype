43683d02a2f8b18d8a0e00cc2acce001fb1c3780
chendehe <chendehe@cmss.chinamobile.com>
2021-10-30 11:14:07 +0800

    添加简单测试类

---
+++ spring-boot-archetype/src/main/java/com/chendehe/controller/TestController.java
@@ -0,0 +1,32 @@
+package com.chendehe.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 修改：
+ *
+ * @author CDH
+ * @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+ * 注释：UserController、UserServiceImpl
+ * @since 2019/7/27 16:10
+ */
+@RestController
+@RequestMapping("/test")
+public final class TestController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
+    /**
+     * 测试.
+     */
+    @GetMapping("/hello")
+    public String hello() {
+        LOGGER.info("test hello");
+        return "Hello World";
+    }
+
+}
