eb79fb049136b58c770db898a86b3eeea18f3326
chendehe <chendehe@cmss.chinamobile.com>
2021-11-02 18:36:28 +0800

    增加灰度标识

--- spring-boot-archetype/src/main/java/com/chendehe/controller/FeignTestController.java
+++ spring-boot-archetype/src/main/java/com/chendehe/controller/FeignTestController.java
@@ -53,7 +53,7 @@ public final class FeignTestController {
             return "Hello feign AND " + hello;
         }
 
-        return "Hello feign";
+        return "Hello feign:" + feignGrayTag;
     }
 
 }
