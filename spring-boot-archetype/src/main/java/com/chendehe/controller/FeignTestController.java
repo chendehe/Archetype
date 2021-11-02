d29191fad953be3528957f18a0d340dd0c9ee180
chendehe <chendehe@cmss.chinamobile.com>
2021-11-02 18:00:27 +0800

    增加灰度标识

--- spring-boot-archetype/src/main/java/com/chendehe/controller/FeignTestController.java
+++ spring-boot-archetype/src/main/java/com/chendehe/controller/FeignTestController.java
@@ -4,6 +4,7 @@ import com.chendehe.service.FeignService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,18 +32,20 @@ public final class FeignTestController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${feign.gray.tag}")
+    private String feignGrayTag;
+
     /**
      * 测试.
      */
     @GetMapping("/test/{type}")
     public String feign(@PathVariable int type) {
         LOGGER.info("test feign");
+        LOGGER.info("type:{}, tag:{}", type, feignGrayTag);
         if (type == 1) {
-            LOGGER.info("type:{}", type);
             String hello = feignService.hello();
             LOGGER.info("test feign resp:{}", hello);
         } else {
-            LOGGER.info("type:{}", type);
             String url = "http://cdh-service.kube-system:1111/test/hello";
             ResponseEntity<String> hello = restTemplate.getForEntity(url, String.class);
             LOGGER.info("test feign resp:{}", hello);
--- spring-boot-archetype/src/main/java/com/chendehe/controller/TestController.java
+++ spring-boot-archetype/src/main/java/com/chendehe/controller/TestController.java
@@ -2,6 +2,7 @@ package com.chendehe.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +21,16 @@ public final class TestController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
 
+    @Value("${feign.gray.tag:\"\"}")
+    private String feignGrayTag;
+
     /**
      * 测试.
      */
     @GetMapping("/hello")
     public String hello() {
         LOGGER.info("test hello");
-        return "Hello World";
+        return "Hello World:" + feignGrayTag;
     }
 
 }
